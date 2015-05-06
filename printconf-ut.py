d85bfd5e8b2f615832c4fb2d1ce4ca36e5131502
asciireign <sebastian.reigber@gmx.net>
2015-05-06 16:27:49 +0200

    blanks removed with .format() and added vlandef function.

--- printconf-ut.py
+++ printconf-ut.py
@@ -2,7 +2,7 @@ import untangle
 config = untangle.parse('settings.xml')
 
 Name = config.data.standort.Name.cdata
-IP = config.data.standort.IP.cdata
+SOIP = config.data.standort.IP.cdata
 
 VLANzahl = config.data.VLANs.Anzahl.cdata
 try: 
@@ -10,10 +10,23 @@ try:
 except ValueError:
 	VLANzahl = 1
 startIP = 101
+portA = 10
+portB = 12
 
 print('Standort: ' + Name)
-print('IP Basis: ' + IP)
+print('IP Basis: ' + SOIP)
+
+def vlandef(startIP,SOIP,portA,portB):
+	print 'vlan', startIP
+	print 'name "VLAN {}"'.format(startIP)
+	print 'untagged {}-{}'.format(portA,portB)
+	print 'ip adress 10.{}.{}.1 255.255.255.0'.format(SOIP,startIP)
+	print 'ip helper-address 10.140.{}.10'.format(SOIP)
+	print 'exit'
 
 for x in range(0,int(VLANzahl)):
-	print "VLAN ", startIP,'mit IP-Netz: 10.'+IP+'.',startIP,'.0 erstellt.'
-	startIP = startIP + 1
+	print 'VLAN {} mit IP-Netz: 10.{}.{}.0 erstellt.'.format(startIP,SOIP,startIP)
+	vlandef(startIP,SOIP,portA,portB)
+	portA = portA+3
+	portB = portB+3
+	startIP = startIP+1
